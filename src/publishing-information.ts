72722bde8eb2cb873f389f0496eb9689aad5d3b5
petergrainger <peter.grainger1@dwp.gsi.gov.uk>
2018-05-20 16:02:51 +0100

    changed to different sound

--- src/publishing-information.ts
+++ src/publishing-information.ts
@@ -1,12 +1,12 @@
 export enum PublishingInformation {
-    APP_NAME = 'Ambient mix: studying at hogwarts',
-    SUMMARY = 'Background ambient noise of the hogwarts common room',
-    NAME = 'studying at hogwarts',
+    APP_NAME = 'Rain on a tin roof',
+    SUMMARY = 'Background ambient noise of rain hitting a tin roof',
+    NAME = 'rain on a tin roof',
     // tslint:disable-next-line:max-line-length
-    DESCRIPTION = 'Immersive background soundtrack to help you concentrate or sleep.  A cozy fireplace, wind rattling against the window while inside there is chatting and studying.  Plays continuously until requested to stop.',
+    DESCRIPTION = 'Immersive background soundtrack to help you concentrate or sleep.  Rain hitting a tin roof with sounds of thunder in the distance.',
     TESTING_INSTRUCTIONS = 'n/a',
     CATEGORY = 'HEALTH_AND_FITNESS',
-    EXAMPLE_PHRASES_1 = 'Alexa open studying at hogwarts',
-    EXAMPLE_PHRASES_2 = 'Alexa play studying at hogwarts',
-    EXAMPLE_PHRASES_3 = 'Alexa start studying at hogwarts',
+    EXAMPLE_PHRASES_1 = 'Alexa open rain on a tin roof',
+    EXAMPLE_PHRASES_2 = 'Alexa play rain on a tin roof',
+    EXAMPLE_PHRASES_3 = 'Alexa start rain on a tin roof',
 }
--- src/skill-definition/models/streams/alexa-stream.ts
+++ src/skill-definition/models/streams/alexa-stream.ts
@@ -1,6 +1,6 @@
 import { Stream } from "alexa-app/types";
 export class AlexaStream implements Stream {
-    public url =  'https://s3.amazonaws.com/alexa-sounds-pg/studying-at-hogwarts.mp3';
-    public token =  'https://s3.amazonaws.com/alexa-sounds-pg/studying-at-hogwarts.mp3';
+    public url =  'https://s3.amazonaws.com/alexa-sounds-pg/tin-roof.mp3';
+    public token =  'https://s3.amazonaws.com/alexa-sounds-pg/tin-roof.mp3';
     public offsetInMilliseconds = 0;
 }
